b8878fce3fcd5a4380ef4e75a100878b8db375f4
Swizz540 <quentin.g@laposte.net>
2013-03-15 08:18:08 +0100

    Create test.user.js

---
+++ test.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name Test
+// @namespace Test
+// @author Gideon
+// @description Test require
+// @license WTF Public License; http://en.wikipedia.org/wiki/WTF_Public_License
+// @include http://www.dreadcast.net/Main
+// @require https://github.com/Swizz540/DreadCast-Lib/blob/Jquery-Less/dist/dreadcastlib.min.js
+// @compat Firefox, Chrome
+// ==/UserScript==
+
+item("aitl").ouverture(alert("ouvert"));
+item("aitl").fermeture(alert("fermé"));
